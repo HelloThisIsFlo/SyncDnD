2ca40626cc7b6f0726316fb10cce46ae6c92de64
Florian Kempenich <Flori@nKempenich.com>
2020-07-19 00:06:41 +0100

    Useful logging format

--- mac/local_wrapper/sandbox/launchd_demo/demo.js
+++ mac/local_wrapper/sandbox/launchd_demo/demo.js
@@ -1,1 +1,15 @@
-console.log("Hello", Date.now())
+const pad2Digits = (num) => String(num).padStart(2, "0");
+const timestamp = () => {
+  const now = new Date();
+  return (
+    "[" +
+    pad2Digits(now.getHours()) +
+    ":" +
+    pad2Digits(now.getMinutes()) +
+    ":" +
+    pad2Digits(now.getSeconds()) +
+    "]"
+  );
+};
+
+console.log(timestamp(), "<== There should be a 20 sec interval between 2 lines");
