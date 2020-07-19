32ec11609b796bb6e15ef4d932603cd828f1377d
Florian Kempenich <Flori@nKempenich.com>
2020-07-19 16:28:07 +0100

    Use logging instead of print statement

--- mac/local_wrapper/local_wrapper.py
+++ mac/local_wrapper/local_wrapper.py
@@ -1,5 +1,28 @@
+import logging
+import sys
 import os
 import paho.mqtt.client as mqtt
+from threading import Thread
+
+
+TOPIC = 'dnd'
+PORT = 6789  # TODO: Remove hardcoding
+DOMAIN = 'floriankempenich.com'
+USER = 'syncdnd'
+PASS = 'pass'
+
+
+def new_logger():
+    logger = logging.getLogger()
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
 
 
 class DnD:
@@ -15,14 +38,6 @@ class DnD:
         os.system('defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean false')
         os.system('killall NotificationCenter')
 
-
-TOPIC = 'dnd'
-PORT = 6789  # TODO: Remove hardcoding
-DOMAIN = 'floriankempenich.com'
-USER = 'syncdnd'
-PASS = 'pass'
-
-
 class MQTTClient:
     def __init__(self, cmd_handler):
         self.cmd_handler = cmd_handler
@@ -33,34 +48,34 @@ class MQTTClient:
         self.paho_client.username_pw_set(USER, PASS)
 
     def connect_and_start_listening(self):
-        print('Connecting to MQTT')
+        logger.info('Connecting to MQTT')
         self.paho_client.connect(host=DOMAIN, port=PORT)
-        # FIXME: When running the loop, all calls to 'print' stop working (only when running throught launchd)
         self.paho_client.loop_forever()
 
     @staticmethod
     def _on_connect(client, _userdata, _flags, _rc):
-        print('Connected to MQTT')
+        logger.info('Connected to MQTT')
         client.subscribe(TOPIC)
-        print(f'Subscribed to {TOPIC}')
+        logger.info(f'Subscribed to {TOPIC}')
 
     def on_message(self, _client, _userdata, msg):
         command = msg.payload.decode()
-        print(f"Just received: '{command}' on '{msg.topic}'")
+        logger.info(f"Just received: '{command}' on '{msg.topic}'")
         self.cmd_handler(command)
 
 
 def handle_cmd_from_mqtt(cmd):
     if cmd == 'ON':
-        print('Turning DnD ON')
+        logger.info('Turning DnD ON')
         DnD.turn_on()
     elif cmd == 'OFF':
-        print('Turning DnD OFF')
+        logger.info('Turning DnD OFF')
         DnD.turn_off()
     else:
-        print(f"Unknown command '{cmd}' - IGNORING")
+        logger.info(f"Unknown command '{cmd}' - IGNORING")
 
 
-print("SyncDnD Local Wrapper is starting...")
+logger = new_logger()
+logger.info("SyncDnD Local Wrapper is starting...")
 mqtt_client = MQTTClient(cmd_handler=handle_cmd_from_mqtt)
 mqtt_client.connect_and_start_listening()
