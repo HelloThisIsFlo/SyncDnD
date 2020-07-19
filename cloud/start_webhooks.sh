db7b2015c0c3328d305edfe4a1019ac70e090073
Florian Kempenich <Flori@nKempenich.com>
2020-07-19 20:10:03 +0100

    Implement webhooks

--- cloud/start_webhooks.sh
+++ cloud/start_webhooks.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo 'todo'
+# Load config
+source $DIR/../config.env
+
+# Inject config.json
+cat $DIR/webhooks/config.json.template |
+    sed "s|%DOMAIN|$DOMAIN|" |
+    sed "s|%MQTT_PORT|$MQTT_PORT|" |
+    sed "s|%MQTT_PASS|$MQTT_PASS|" \
+        >$DIR/webhooks/config.json
+
+# Start Webhooks
+export WEBHOOKS_PORT=3333 # TODO: Put in cfg & update readme
+docker-compose -f $DIR/webhooks/docker-compose.yml up --build -d
+
+# Clean up injected config.json
+rm $DIR/webhooks/config.json
---
+++ cloud/webhooks/webhooks.py
@@ -0,0 +1,52 @@
+import paho.mqtt.client as mqtt
+from flask import Flask
+import json
+
+
+def load_config():
+    with open('./config.json') as config_file:
+        return json.loads(config_file.read())
+
+class MQTTClient:
+    def __init__(self):
+        self.paho_client = mqtt.Client(client_id='DnD-Webhooks')
+        self.paho_client.on_connect = self._on_connect
+        self.paho_client.tls_set()
+        self.paho_client.username_pw_set(
+            config['mqtt']['user'], config['mqtt']['pass'])
+
+    def connect(self):
+        print('Connecting to MQTT')
+        self.paho_client.connect(
+            host=config['mqtt']['domain'], 
+            port=config['mqtt']['port'])
+
+    @staticmethod
+    def _on_connect(client, _userdata, _flags, _rc):
+        logger.info('Connected to MQTT')
+
+    def send_ON(self):
+        self._send_msg('ON')
+
+    def send_OFF(self):
+        self._send_msg('OFF')
+
+    def _send_msg(self, msg):
+        self.paho_client.publish(config['mqtt']['topic'], msg)
+
+
+
+config = load_config()
+app = Flask(__name__)
+mqtt_client = MQTTClient()
+mqtt_client.connect()
+
+@app.route('/dnd/on')
+def dnd_turn_on():
+    mqtt_client.send_ON()
+    return 'DnD turned ON'
+
+@app.route('/dnd/off')
+def dnd_turn_off():
+    mqtt_client.send_OFF()
+    return 'DnD turned OFF'
