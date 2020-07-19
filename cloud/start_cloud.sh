ce2484b8ac1a58203b0ddb463eadb78f8bf998f4
Florian Kempenich <Flori@nKempenich.com>
2020-07-19 19:29:32 +0000

    Fix cloud start/stop scripts

--- cloud/start_cloud.sh
+++ cloud/start_cloud.sh
@@ -4,7 +4,31 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # Load config
 source $DIR/../config.env
 
-# Stop MQTT & Webhooks
+####### Webhooks ########################################################################################
+# Inject config.json
+cat $DIR/webhooks/config.json.template |
+    sed "s|%DOMAIN|$DOMAIN|" |
+    sed "s|%MQTT_PORT|$MQTT_PORT|" |
+    sed "s|%MQTT_PASS|$MQTT_PASS|" \
+        >$DIR/webhooks/config.json
+####### Webhooks ########################################################################################
+
+####### MQTT ############################################################################################
+# Create mosquitto config
+sed "s/DOMAIN/$DOMAIN/" $DIR/mqtt/config/mosquitto.conf.template >$DIR/mqtt/config/mosquitto.conf
+
+# Create password file
+MQTT_USER=syncdnd
+echo "$MQTT_USER:$MQTT_PASS" >$DIR/mqtt/config/password
+
+# Encrypt password file
+docker run -it -v $DIR/mqtt/config/password:/password eclipse-mosquitto mosquitto_passwd -U password
+####### MQTT ############################################################################################
+
+# Start MQTT & Webhooks
 export MQTT_PORT
 export WEBHOOKS_PORT
-docker-compose down
+docker-compose -f $DIR/docker-compose.yml up --build -d
+
+# Clean up injected config.json (webhooks)
+rm $DIR/webhooks/config.json
--- cloud/stop_cloud.sh
+++ cloud/stop_cloud.sh
@@ -4,31 +4,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # Load config
 source $DIR/../config.env
 
-####### Webhooks ########################################################################################
-# Inject config.json
-cat $DIR/webhooks/config.json.template |
-    sed "s|%DOMAIN|$DOMAIN|" |
-    sed "s|%MQTT_PORT|$MQTT_PORT|" |
-    sed "s|%MQTT_PASS|$MQTT_PASS|" \
-        >$DIR/webhooks/config.json
-####### Webhooks ########################################################################################
-
-####### MQTT ############################################################################################
-# Create mosquitto config
-sed "s/DOMAIN/$DOMAIN/" $DIR/mqtt/config/mosquitto.conf.template >$DIR/mqtt/config/mosquitto.conf
-
-# Create password file
-MQTT_USER=syncdnd
-echo "$MQTT_USER:$MQTT_PASS" >$DIR/mqtt/config/password
-
-# Encrypt password file
-docker run -it -v $DIR/mqtt/config/password:/password eclipse-mosquitto mosquitto_passwd -U password
-####### MQTT ############################################################################################
-
-# Start MQTT & Webhooks
+# Stop MQTT & Webhooks
 export MQTT_PORT
 export WEBHOOKS_PORT
-docker-compose up --build -d
-
-# Clean up injected config.json (webhooks)
-rm $DIR/webhooks/config.json
+docker-compose -f $DIR/docker-compose.yml down
