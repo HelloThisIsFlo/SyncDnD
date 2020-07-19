f6b00dc735db00dc42049d30a67525a850aa3591
Florian Kempenich <Flori@nKempenich.com>
2020-07-19 15:12:53 +0100

    Restore business logic

--- mac/local_wrapper/local_wrapper.py
+++ mac/local_wrapper/local_wrapper.py
@@ -33,7 +33,9 @@ class MQTTClient:
         self.paho_client.username_pw_set(USER, PASS)
 
     def connect_and_start_listening(self):
+        print('Connecting to MQTT')
         self.paho_client.connect(host=DOMAIN, port=PORT)
+        # FIXME: When running the loop, all calls to 'print' stop working (only when running throught launchd)
         self.paho_client.loop_forever()
 
     @staticmethod
@@ -48,26 +50,6 @@ class MQTTClient:
         self.cmd_handler(command)
 
 
-def sandbox():
-    def on_connect(client, _userdata, _flags, _rc):
-        print('Connected to MQTT')
-        client.subscribe(TOPIC)
-        print(f'Subscribed to {TOPIC}')
-
-    def on_message(_client, _userdata, msg):
-        command = msg.payload.decode()
-        print(f"Just received: '{command}' on '{msg.topic}'")
-
-    paho_client = mqtt.Client(client_id='DnD-Local-Wrapper')
-    paho_client.on_connect = on_connect
-    paho_client.on_message = on_message
-    paho_client.tls_set(ca_certs='/etc/ssl/cert.pem')
-    paho_client.username_pw_set(USER, PASS)
-
-    paho_client.connect(host=DOMAIN, port=PORT)
-    paho_client.loop_forever()
-
-
 def handle_cmd_from_mqtt(cmd):
     if cmd == 'ON':
         print('Turning DnD ON')
@@ -79,25 +61,6 @@ def handle_cmd_from_mqtt(cmd):
         print(f"Unknown command '{cmd}' - IGNORING")
 
 
-# mqtt_client = MQTTClient(cmd_handler=handle_cmd_from_mqtt)
-# mqtt_client.connect_and_start_listening()
-
-def debug_daemon():
-    from datetime import datetime
-
-    def timestamp():
-        now = datetime.now()
-        return now.strftime("[%H:%M:%S]")
-
-    print(timestamp() + " <== There should be a 20 sec interval between 2 lines")
-    print(os.system('pwd'))
-    print(os.system('ls -la'))
-
-    print('turning on dnd')
-    os.system(
-        'defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean true')
-    os.system('defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturbDate -date "`date -u +\"%Y-%m-%d %H:%M:%S +0000\"`"')
-    os.system('killall NotificationCenter')
-
-
-debug_daemon()
+print("SyncDnD Local Wrapper is starting...")
+mqtt_client = MQTTClient(cmd_handler=handle_cmd_from_mqtt)
+mqtt_client.connect_and_start_listening()
